1feb5033fc13750d0ca2caaa41238a0ce8166104
George Groh <george.groh@gmail.com>
2014-04-20 11:42:15 -0700

    Refactored question_follower model

--- question_follower.rb
+++ question_follower.rb
@@ -3,61 +3,76 @@
 end
 
 class QuestionFollower
-  attr_accessor :id, :question_id, :follower_id
 
   def self.find_by_id(id)
-    query = QuestionsDatabase.instance.execute(<<-SQL, :id => id)
-    SELECT *
-    FROM question_followers
-    WHERE id = :id
+    follower_query = QuestionsDatabase.get_first_row(<<-SQL, id: id)
+      SELECT 
+        *
+      FROM 
+        question_followers
+      WHERE 
+        id = :id
     SQL
 
-    QuestionFollower.new(query[0])
+    QuestionFollower.new(follower_query)
   end
 
   def self.followers_for_question_id(question_id)
-    queries = QuestionsDatabase.instance.execute(<<-SQL, question_id)
-    SELECT u.id, u.fname, u.lname
-    FROM
-      users u JOIN question_followers q
-    ON
-      u.id = q.follower_id
-    WHERE
-      q.question_id = ?
+    user_queries = QuestionsDatabase.execute(<<-SQL, question_id: question_id)
+      SELECT 
+        u.*
+      FROM
+        users u 
+      JOIN 
+        question_followers q
+      ON
+        u.id = q.follower_id
+      WHERE
+        q.question_id = :question_id
     SQL
-    queries.map { |query| User.new(query) }
+    
+    user_queries.map { |query| User.new(query) }
   end
 
   def self.followed_questions_for_user_id(user_id)
-    queries = QuestionsDatabase.instance.execute(<<-SQL, user_id)
-    SELECT q.id, q.title, q.body, q.author_id
-    FROM
-      questions q JOIN question_followers f
-    ON
-      q.id = f.question_id
-    WHERE
-      f.follower_id = ?
+    question_queries = QuestionsDatabase.execute(<<-SQL, user_id: user_id)
+      SELECT 
+        q.*
+      FROM
+        questions q 
+      JOIN 
+        question_followers f
+      ON
+        q.id = f.question_id
+      WHERE
+        f.follower_id = :user_id
     SQL
-    queries.map { |query| Question.new(query) }
+    
+    question_queries.map { |query| Question.new(query) }
   end
 
   def self.most_followed_questions(n)
-    queries = QuestionsDatabase.instance.execute(<<-SQL, n)
-    SELECT *
-    FROM questions
+    question_queries = QuestionsDatabase.execute(<<-SQL, n: n)
+    SELECT 
+      *
+    FROM 
+      questions
     WHERE id IN (
-      SELECT question_id
+      SELECT 
+        question_id
       FROM
         question_followers
       GROUP BY
         question_id
       ORDER BY
-      -COUNT(follower_id)
-      LIMIT ?
+        -COUNT(follower_id)
+      LIMIT :n
     )
     SQL
-    queries.map { |query| Question.new(query) }
+    question_queries.map { |query| Question.new(query) }
   end
+
+  attr_accessor :id, :question_id, :follower_id
 
   def initialize(options = {})
     @id = options["id"]
